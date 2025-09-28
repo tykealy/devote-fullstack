1ad7229b6c75bec75d8c58388022b486af7ab32d
tykealy <tykeaboyloy@gmail.com>
2025-09-28 23:02:21 +0700

    add daisy ui theme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" data-theme="lofi">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
